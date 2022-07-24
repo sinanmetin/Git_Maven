3b4513f0e978283f270938bba4ff1bdbdfba8aa8
Sinan Metin <sinanmetin53@gmail.com>
2022-07-24 12:14:51 +0200

    started IgnoreTest file committing

---
+++ src/main/java/Ignore_Test1.java
@@ -0,0 +1,10 @@
+public class Ignore_Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("bu dosya ignore olmali");
+        System.out.println("bu dosya ignore oldu güzel oldu");
+
+        System.out.println("started committing");
+
+    }
+}
