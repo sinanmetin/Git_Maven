c5cab1f60f6ca34c0532e6c9d4041fd9032634a9
Sinan Metin <sinanmetin53@gmail.com>
2022-07-24 13:00:09 +0200

    some changes on UpdateTest

--- src/main/java/UpdateTest.java
+++ src/main/java/UpdateTest.java
@@ -2,5 +2,6 @@ public class UpdateTest {
     public static void main(String[] args) {
         System.out.println("hello github update test");
         System.out.println("it is on remote repository");
+        System.out.println("yes it is on my local repository");
     }
 }
