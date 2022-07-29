844b443959fb66f9f857c132c8a3c1c059074cc9
Sinan Metin <sinanmetin53@gmail.com>
2022-07-29 11:05:11 +0200

    feature E new class

---
+++ src/main/java/feature_E/Feature_E.java
@@ -0,0 +1,10 @@
+package feature_E;
+
+import feature_D.Feature_DA;
+
+public class Feature_E extends Feature_DA {
+    public static void main(String[] args) {
+
+        System.out.println();
+    }
+}
